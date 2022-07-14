db3227cebb9d9217efd3dee09c20ded0c6cd54a3
mmarko88 <mmarko88@users.noreply.github.com>
2022-07-14 08:38:03 +0000

    deploy: 4f06ae298df9a8ed58f500f0d531273b7c328dbe

--- en/js/bundle.8766b79ce7de0790e7d43c83c9d9175bed66d1d51af0f4695597cb4ee02aecfec220345eb8326437391bf7d40284c17b3ceee6001b552155a13807fc8b427264.js
+++ en/js/bundle.8766b79ce7de0790e7d43c83c9d9175bed66d1d51af0f4695597cb4ee02aecfec220345eb8326437391bf7d40284c17b3ceee6001b552155a13807fc8b427264.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'https://mmarko88.github.io';
+const baseURL = 'http://www.professionaldev.pro';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
