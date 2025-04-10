7c80e0456ea751f0a658a0d0eaaffa3e0ceeb471
Marko Milenkovic <marko@Markos-MacBook-Pro.local>
2025-04-10 21:40:19 +0200

    first commit

---
+++ src/main/java/com/marko/tradingbot/App.java
@@ -0,0 +1,13 @@
+package com.marko.tradingbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
